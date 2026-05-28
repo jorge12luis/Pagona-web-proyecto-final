d1abdf6f7b22cbe323697c64499a44f5da5166ab
Julio_Martinez <julio763284@gmail.com>
2026-05-28 18:06:36 -0500

    editar imagen de perfil

--- js/editar_perfil.js
+++ js/editar_perfil.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     cargarUsuarioDesdeBackend();
 
-    eventos();
+    mostrarOcultarPassword();
+
+    mostrarFotoPerfil();
 
 });
 
@@ -37,8 +39,9 @@ function cargarUsuarioDesdeBackend() {
             document.getElementById("inputApellido").value = u.apellido || "";
             document.getElementById("inputemail").value = u.correo || "";
             document.getElementById("inputcelular").value = u.numero_telefono || "";
-            document.getElementById("inputdate").value = u.fecha_nacimiento || "";
+            document.getElementById("inputdate").value = u.fecha_nacimiento.split("T")[0];
             document.getElementById("inputpassword").value = u.contrasena || "";
+            
 
         } else {
             console.log("No se encontró usuario");
@@ -46,4 +49,82 @@ function cargarUsuarioDesdeBackend() {
 
     })
     .catch(err => console.log(err));
+}
+
+function mostrarOcultarPassword(){
+
+    const togglePassword = document.getElementById("togglePassword");
+    const inputPassword = document.getElementById("inputpassword");
+
+    togglePassword.addEventListener("click", () => {
+
+        if(inputPassword.type === "password"){
+
+            inputPassword.type = "text";
+
+            togglePassword.classList.remove("fa-eye");
+            togglePassword.classList.add("fa-eye-slash");
+
+        }else{
+
+            inputPassword.type = "password";
+
+            togglePassword.classList.remove("fa-eye-slash");
+            togglePassword.classList.add("fa-eye");
+
+        }
+
+    });
+
+}
+
+function mostrarFotoPerfil(){
+
+    const btnEditarFoto =
+    document.getElementById("btnEditarFoto");
+
+    const inputFoto =
+    document.getElementById("inputFoto");
+
+    btnEditarFoto.addEventListener("click", () => {
+
+        inputFoto.click();
+
+    });
+
+    inputFoto.addEventListener("change", (e) => {
+
+        const archivo = e.target.files[0];
+
+        if(!archivo) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function(event){
+
+            const imagenes =
+            document.querySelectorAll(".fotoPerfil");
+
+            const iconos =
+            document.querySelectorAll(".iconoUsuario");
+
+            imagenes.forEach(img => {
+
+                img.src = event.target.result;
+                img.style.display = "block";
+
+            });
+
+            iconos.forEach(icono => {
+
+                icono.style.display = "none";
+
+            });
+
+        };
+
+        reader.readAsDataURL(archivo);
+
+    });
+
 }
